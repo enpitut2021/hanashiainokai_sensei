73f8363c0e5c6e9bc4f7d95c40e27e3cd74e8ff2
Alta <60193698+Altair-item@users.noreply.github.com>
2021-12-01 12:46:21 +0900

    json取得ができる

---
+++ getjson.py
@@ -0,0 +1,12 @@
+import requests, json
+
+url = requests.get("https://jsonplaceholder.typicode.com/users")
+text = url.text
+
+data = json.loads(text)
+
+user = data[0]
+print(user['name'])
+
+address = user['address']
+print(address)
